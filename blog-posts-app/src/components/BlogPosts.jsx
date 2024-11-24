5a938ba04427494521bef694f3e7ebd90cbbb64c
AmoInno <amogelangmashishi12@gmail.com>
2024-11-24 16:59:01 +0200

    initialise react and fetch& display data

---
+++ blog-posts-app/src/components/BlogPosts.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+const BlogPosts = () => {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Data fetching failed");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setPosts(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error.message); // Save the error message to state
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      <h1>Blog Posts</h1>
+      {posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogPosts;
